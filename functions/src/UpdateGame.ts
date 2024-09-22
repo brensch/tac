993cf2a7201f42f15c590c84e7c030386794e768
brensch <brendolini@protonmail.com>
2024-09-21 18:59:20 -0700

    config hell

--- functions/src/UpdateGame.ts
+++ functions/src/UpdateGame.ts
@@ -1,28 +1,38 @@
 import * as functions from "firebase-functions"
 import * as admin from "firebase-admin"
+import * as logger from "firebase-functions/logger"
 import { GameState, Move } from "@shared/types/Game"
 
 admin.initializeApp()
 
-exports.onMoveCreated = functions.firestore
+export const onMoveCreated = functions.firestore
   .document("games/{gameID}/privateMoves/{moveID}")
   .onCreate(async (snap, context) => {
     const moveData = snap.data() as Move
     const { gameID } = context.params
+
+    // Log the game ID and move data
+    logger.info(`Processing move for gameID: ${gameID}`, { moveData })
 
     const gameRef = admin.firestore().collection("games").doc(gameID)
     const gameDoc = await gameRef.get()
     const gameData = gameDoc.data() as GameState
 
     if (!gameData) {
-      console.error("Game not found")
+      logger.error("Game not found", { gameID })
       return
     }
+
+    // Log current game state
+    logger.info("Game data retrieved", { gameData })
 
     // Update the hasMoved array in the game document
     await gameRef.update({
       hasMoved: admin.firestore.FieldValue.arrayUnion(moveData.playerID),
     })
+
+    // Log player move
+    logger.info(`Player ${moveData.playerID} has moved`, { gameID })
 
     // Check if all players have moved
     const allPlayersMoved = gameData.playerIDs.every((playerID) =>
@@ -42,12 +52,11 @@ exports.onMoveCreated = functions.firestore
       // Apply moves to the board
       playerMovesSnapshot.docs.forEach((doc) => {
         const move = doc.data() as Move
-        // const playerDocRef = admin
-        //   .firestore()
-        //   .collection("users")
-        //   .doc(move.playerID)
         newBoard[move.move] = move.playerID // Put the player's ID in the board position
       })
+
+      // Log board update
+      logger.info("All players moved, updating the board", { newBoard })
 
       // Update the game board and increment the round number
       await gameRef.update({
@@ -55,5 +64,8 @@ exports.onMoveCreated = functions.firestore
         currentRound: gameData.currentRound + 1,
         hasMoved: [], // Reset hasMoved for the next round
       })
+
+      // Log round update
+      logger.info(`Round ${gameData.currentRound} completed`, { gameID })
     }
   })
